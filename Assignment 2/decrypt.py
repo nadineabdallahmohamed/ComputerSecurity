ac94cca0642fe19dcb4b79e91bb06bcc70b6f8a2
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2020-05-04 02:13:02 +0200

    Update decrypt.py

--- Assignment 2/decrypt.py
+++ Assignment 2/decrypt.py
@@ -9,34 +9,32 @@ def F(w):
    Return the original text after decrypting it
 '''
 
-def decrypt(block):    
-    a, b, c, d = unpack('<4I', block)
-    for i in range(32):       
-        tmp = a
-        d = d ^ 1337
-        a = c ^ (F(d | F(d) ^ d))
-        b = b ^ (F(d ^ F(a) ^ (d | a)))
-        c = tmp ^ (F(d | F(b ^ F(a)) ^ F(d | b) ^ a))
-        
-        tmp = a
-        a = d ^ 31337
-        d = c ^ (F(a | F(a) ^ a))
-        c = b ^ (F(a ^ F(d) ^ (a | d)))
-        b = tmp ^ (F(a | F(c ^ F(d)) ^ F(a | c) ^ d))
-    
-    pk = pack('<4I', a, b, c, d)
-    return pk 
+def decrypt_ciphers(block_cipher):    
+    int1, int2, int3, int4 = unpack('<4I', block_cipher)    
+    for idx in range(32):       
+        tmp = int1
+        int4 = int4 ^ 1337
+        int1 = int3 ^ (F(int4 | F(int4) ^ int4))
+        int2 = int2 ^ (F(int4 ^ F(int1) ^ (int4 | int1)))
+        int3 = tmp ^ (F(int4 | F(int2 ^ F(int1)) ^ F(int4 | int2) ^ int1))     
+        tmp = int1
+        int1 = int4 ^ 31337
+        int4 = int3 ^ (F(int1 | F(int1) ^ int1))
+        int3 = int2 ^ (F(int1 ^ F(int4) ^ (int1 | int4)))
+        int2 = tmp ^ (F(int1 | F(int3 ^ F(int4)) ^ F(int1 | int3) ^ int4))    
+    bytes = pack('<4I', int1, int2, int3, int4)
+    return bytes 
       
 ''' 
     Open the "flag.enc" file in binary format "rb" mode for reading 
     iterate over every 16 bytes of the ciphertext and print out the original text
  '''
-enc = open('flag.enc','rb').read()
+encrypted_text = open('flag.enc','rb').read()
 
 def edit_text(txt):
     new_str = strg.format(txt)[2:-1]
     return new_str
     
 
-dec = "".join(edit_text(decrypt(enc[i:i+16])) for i in range(0,len(enc), 16))
-print (dec)
+decrypted_text = "".join(edit_text(decrypt_ciphers(encrypted_text[idx:idx+16])) for idx in range(0,len(encrypted_text), 16))
+print (decrypted_text)
